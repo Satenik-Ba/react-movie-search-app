3a20b23f94dd35f258ec0d326c64690cc05f137f
Miqo95 <gmiqael@bk.ru>
2021-10-10 14:46:08 +0400

    Acord css

--- src/components/Header/AcordionCountryCategories.js
+++ src/components/Header/AcordionCountryCategories.js
@@ -5,8 +5,19 @@ import InputLabel from "@mui/material/InputLabel";
 import MenuItem from "@mui/material/MenuItem";
 import FormControl from "@mui/material/FormControl";
 import Select from "@mui/material/Select";
+import { makeStyles } from "@mui/styles";
+
+const useStyles = makeStyles(() => {
+  return {
+    root: {
+      backgroundColor: "#171c2c !important",
+      color: "#d1d2d6 !important",
+    },
+  };
+});
 
 function AcordionCountryCategories() {
+  const classes = useStyles();
   const [age, setAge] = React.useState("");
   const handleChange = (event) => {
     setAge(event.target.value);
@@ -16,8 +27,11 @@ function AcordionCountryCategories() {
     <Typography variant="h5" component="div" sx={{ flexGrow: 1 }}>
       <Box sx={{ minWidth: 50 }}>
         <FormControl fullWidth>
-          <InputLabel id="demo-simple-select-label">Films</InputLabel>
+          <InputLabel className={classes.root} id="demo-simple-select-label">
+            Films
+          </InputLabel>
           <Select
+            className={classes.root}
             labelId="demo-simple-select-label"
             id="demo-simple-select"
             value={age}
@@ -27,7 +41,7 @@ function AcordionCountryCategories() {
             <MenuItem value={10}>Armenian Films</MenuItem>
             <MenuItem value={20}>Russian Films</MenuItem>
             <MenuItem value={30}>Artasahmanyan Films</MenuItem>
-            <MenuItem value={30}>Film Armenian Translate</MenuItem>
+            <MenuItem value={40}>Film Armenian Translate</MenuItem>
           </Select>
         </FormControl>
       </Box>
