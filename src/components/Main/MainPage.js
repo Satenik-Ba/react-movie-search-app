839e8bedb808ff52cbba488793445a41bf74d82a
Miqo95 <gmiqael@bk.ru>
2021-10-13 18:29:31 +0400

    carusel + footer

--- src/components/Main/MainPage.js
+++ src/components/Main/MainPage.js
@@ -1,22 +1,35 @@
-import React, { useEffect, useState } from 'react';
-import Movie from './Movie';
-import { POPULAR_MOVIES_API } from '../../constants/APIs';
+import React, { useEffect, useState } from "react";
+import Movie from "./Movie";
+import { POPULAR_MOVIES_API } from "../../constants/APIs";
+import CarouselFilms from "./CarouselFilms";
+import { makeStyles } from "@mui/styles";
+
+const useStyles = makeStyles(() => {
+  return {
+    root: {
+      backgroundColor: "#232A3E",
+    },
+  };
+});
 
 function MainPage() {
+  const classes = useStyles();
   const [featuredMovies, setFeaturedMovies] = useState([]);
   useEffect(() => {
     fetch(POPULAR_MOVIES_API)
       .then((response) => response.json())
       .then((result) => {
-        console.log(result, 'Movie RESULT');
+        console.log(result, "Movie RESULT");
         setFeaturedMovies(result.results);
       });
   }, []);
 
   return (
-    <div>
+    <div className={classes.root}>
+      <div>
+        <CarouselFilms />
+      </div>
       <h1>Featured Movies</h1>
-
       {featuredMovies.map((movie) => (
         <Movie
           key={movie.id}
