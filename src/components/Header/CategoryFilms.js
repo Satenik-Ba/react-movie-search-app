c1ded042db858e5e914b88f3e4945081ea4f3ae3
ArakelyanVardan <vardanarakelyan90@gmail.com>
2021-10-19 22:23:18 +0400

    Category map - ov

--- src/components/Header/CategoryFilms.js
+++ src/components/Header/CategoryFilms.js
@@ -1,4 +1,4 @@
-import * as React from "react";
+import React, { useEffect } from "react";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import InputLabel from "@mui/material/InputLabel";
@@ -8,6 +8,7 @@ import Select from "@mui/material/Select";
 import { makeStyles } from "@mui/styles";
 import { useDispatch } from "react-redux";
 import { CatValueAction } from "../redux/categoryValue";
+import { CAREGORIES_MOVIES_API } from "../../constants/APIs";
 
 const useStyles = makeStyles(() => {
   return {
@@ -29,6 +30,15 @@ function CategoryFilms() {
   const handleChange = (event) => {
     setAge(event.target.value);
   };
+  const [categories, setCategories] = React.useState([]);
+
+  useEffect(() => {
+    fetch(CAREGORIES_MOVIES_API)
+      .then((response) => response.json())
+      .then((result) => {
+        setCategories(result.genres);
+      });
+  }, []);
 
   return (
     <Typography variant="h5" component="div" sx={{ flexGrow: 1 }}>
@@ -45,246 +55,19 @@ function CategoryFilms() {
             label="Films"
             onChange={handleChange}
           >
-            <MenuItem
-              value={0}
-              onClick={() => {
-                dispatch(
-                  CatValueAction.changeValue({
-                    catValue: false,
-                  })
-                );
-              }}
-            >
-              All categories
-            </MenuItem>
-            <MenuItem
-              value={28}
-              onClick={() => {
-                dispatch(
-                  CatValueAction.changeValue({
-                    catValue: "28",
-                  })
-                );
-              }}
-            >
-              Action
-            </MenuItem>
-            <MenuItem
-              value={12}
-              onClick={() => {
-                dispatch(
-                  CatValueAction.changeValue({
-                    catValue: "12",
-                  })
-                );
-              }}
-            >
-              Adventure
-            </MenuItem>
-            <MenuItem
-              value={16}
-              onClick={() => {
-                dispatch(
-                  CatValueAction.changeValue({
-                    catValue: "16",
-                  })
-                );
-              }}
-            >
-              Animation
-            </MenuItem>
-            <MenuItem
-              value={35}
-              onClick={() => {
-                dispatch(
-                  CatValueAction.changeValue({
-                    catValue: "35",
-                  })
-                );
-              }}
-            >
-              Comedy
-            </MenuItem>
-            <MenuItem
-              value={80}
-              onClick={() => {
-                dispatch(
-                  CatValueAction.changeValue({
-                    catValue: "80",
-                  })
-                );
-              }}
-            >
-              Crime
-            </MenuItem>
-            <MenuItem
-              value={99}
-              onClick={() => {
-                dispatch(
-                  CatValueAction.changeValue({
-                    catValue: "99",
-                  })
-                );
-              }}
-            >
-              Documentary
-            </MenuItem>
-            <MenuItem
-              value={18}
-              onClick={() => {
-                dispatch(
-                  CatValueAction.changeValue({
-                    catValue: "18",
-                  })
-                );
-              }}
-            >
-              Drama
-            </MenuItem>
-            <MenuItem
-              value={10751}
-              onClick={() => {
-                dispatch(
-                  CatValueAction.changeValue({
-                    catValue: "10751",
-                  })
-                );
-              }}
-            >
-              Family
-            </MenuItem>
-            <MenuItem
-              value={14}
-              onClick={() => {
-                dispatch(
-                  CatValueAction.changeValue({
-                    catValue: "14",
-                  })
-                );
-              }}
-            >
-              Fantasy
-            </MenuItem>
-            <MenuItem
-              value={36}
-              onClick={() => {
-                dispatch(
-                  CatValueAction.changeValue({
-                    catValue: "36",
-                  })
-                );
-              }}
-            >
-              History
-            </MenuItem>
-            <MenuItem
-              value={27}
-              onClick={() => {
-                dispatch(
-                  CatValueAction.changeValue({
-                    catValue: "27",
-                  })
-                );
-              }}
-            >
-              Horror
-            </MenuItem>
-            <MenuItem
-              value={10402}
-              onClick={() => {
-                dispatch(
-                  CatValueAction.changeValue({
-                    catValue: "10402",
-                  })
-                );
-              }}
-            >
-              Music
-            </MenuItem>
-            <MenuItem
-              value={9648}
-              onClick={() => {
-                dispatch(
-                  CatValueAction.changeValue({
-                    catValue: "9648",
-                  })
-                );
-              }}
-            >
-              Mystery
-            </MenuItem>
-            <MenuItem
-              value={10749}
-              onClick={() => {
-                dispatch(
-                  CatValueAction.changeValue({
-                    catValue: "10729",
-                  })
-                );
-              }}
-            >
-              Romance
-            </MenuItem>
-            <MenuItem
-              value={878}
-              onClick={() => {
-                dispatch(
-                  CatValueAction.changeValue({
-                    catValue: "878",
-                  })
-                );
-              }}
-            >
-              Science Fiction
-            </MenuItem>
-            <MenuItem
-              value={10770}
-              onClick={() => {
-                dispatch(
-                  CatValueAction.changeValue({
-                    catValue: "10770",
-                  })
-                );
-              }}
-            >
-              TV Movie
-            </MenuItem>
-            <MenuItem
-              value={53}
-              onClick={() => {
-                dispatch(
-                  CatValueAction.changeValue({
-                    catValue: "53",
-                  })
-                );
-              }}
-            >
-              Thriller
-            </MenuItem>
-            <MenuItem
-              value={10752}
-              onClick={() => {
-                dispatch(
-                  CatValueAction.changeValue({
-                    catValue: "10752",
-                  })
-                );
-              }}
-            >
-              War
-            </MenuItem>
-            <MenuItem
-              value={37}
-              onClick={() => {
-                dispatch(
-                  CatValueAction.changeValue({
-                    catValue: "37",
-                  })
-                );
-              }}
-            >
-              Western
-            </MenuItem>
+            {categories.map((category) => (
+              <MenuItem
+                onClick={() => {
+                  dispatch(
+                    CatValueAction.changeValue({
+                      catValue: category.id,
+                    })
+                  );
+                }}
+              >
+                {category.name}
+              </MenuItem>
+            ))}
           </Select>
         </FormControl>
       </Box>
