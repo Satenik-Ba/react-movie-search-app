d8a4752fe2b22a3260efdb5dd2ed4db809af27a0
Satenik-Ba <satenik04@hotmail.com>
2021-11-29 12:48:23 -0800

    UI improvements to the movie/tv show info page

--- src/components/Main/Comments.js
+++ src/components/Main/Comments.js
@@ -1,53 +1,54 @@
-import React, { useState, useEffect } from "react";
-import { SIGNIN_ROUTE } from "../../constants/routes";
-import { useHistory } from "react-router-dom";
-import { makeStyles } from "@mui/styles";
-import TextareaAutosize from "@mui/material/TextareaAutosize";
-import Button from "@mui/material/Button";
-import { useSelector } from "react-redux";
-import { doc, updateDoc, onSnapshot } from "firebase/firestore";
-import { firestore } from "../../firebase";
-import { arrayUnion } from "@firebase/firestore";
-import Login from "../Authenticataion/Login";
-import { DialogContentText, DialogContent, Dialog } from "@mui/material";
-import { styled } from "@mui/material/styles";
-import Box from "@mui/material/Box";
-import Paper from "@mui/material/Paper";
-import Grid from "@mui/material/Grid";
+import React, { useState, useEffect } from 'react';
+import { SIGNIN_ROUTE } from '../../constants/routes';
+import { useHistory } from 'react-router-dom';
+import { makeStyles } from '@mui/styles';
+import TextareaAutosize from '@mui/material/TextareaAutosize';
+import Button from '@mui/material/Button';
+import { useSelector } from 'react-redux';
+import { doc, updateDoc, onSnapshot } from 'firebase/firestore';
+import { firestore } from '../../firebase';
+import { arrayUnion } from '@firebase/firestore';
+import Login from '../Authenticataion/Login';
+import { DialogContentText, DialogContent, Dialog } from '@mui/material';
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
 
 const Item = styled(Paper)(({ theme }) => ({
   ...theme.typography.body2,
   padding: theme.spacing(1),
-  textAlign: "center",
+  textAlign: 'center',
   color: theme.palette.text.secondary,
 }));
 
 const useStyles = makeStyles({
   rootBtn: {
-    color: "#C52D3D !important",
-    fontSize: "18px !important",
+    color: '#C52D3D !important',
+    fontSize: '18px !important',
+    border: '1px solid #C52D3D !important'
   },
   divComentUsers: {
-    width: "80%",
-    minHeight: "200px",
-    border: "1px #222222 solid",
-    textAlign: "left",
-    margin: "auto",
-    color: "orange",
-    lineHeight: "0px",
-    fontWeight: "bold",
+    width: '80%',
+    minHeight: '200px',
+    border: '1px #222222 solid',
+    textAlign: 'left',
+    margin: 'auto',
+    color: 'orange',
+    lineHeight: '0px',
+    fontWeight: 'bold',
   },
   comentValueUser: {
-    fontWeight: "bold",
-    color: "#D1D2D6",
-    minHeight: "20px",
-    fontSize: "20px",
-    lineHeight: "10px",
+    fontWeight: 'bold',
+    color: '#D1D2D6',
+    minHeight: '20px',
+    fontSize: '20px',
+    lineHeight: '10px',
   },
 
   spanTime: {
-    color: "#C52D3D",
-    fontSize: "15px",
+    color: '#C52D3D',
+    fontSize: '15px',
   },
 });
 
@@ -56,18 +57,18 @@ export default function Comments({ movie1 }) {
   const history = useHistory();
   const comentUserName = useSelector((state) => state.userInfo.userName);
   const comentUserId = useSelector((state) => state.userInfo.userId);
-  const [commentValue, setCommentValue] = useState("");
+  const [commentValue, setCommentValue] = useState('');
   const [loadingComentPage, setLoadingComentPage] = useState();
   const [isEmpty, setIsEmpty] = useState(true);
   const timestamp = Date.now();
   const [open, setOpen] = useState(false);
-  const newDataInComent = new Intl.DateTimeFormat("en-US", {
-    year: "numeric",
-    month: "2-digit",
-    day: "2-digit",
-    hour: "2-digit",
-    minute: "2-digit",
-    second: "2-digit",
+  const newDataInComent = new Intl.DateTimeFormat('en-US', {
+    year: 'numeric',
+    month: '2-digit',
+    day: '2-digit',
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
   }).format(timestamp);
 
   const onHandleChange = (event) => {
@@ -81,8 +82,8 @@ export default function Comments({ movie1 }) {
   };
 
   async function onAddItem() {
-    if (comentUserName !== "") {
-      if (commentValue.trim() !== "") {
+    if (comentUserName !== '') {
+      if (commentValue.trim() !== '') {
         const commentsRef = doc(firestore, `/comments/${movie1.id}`);
         await updateDoc(commentsRef, {
           movieComments: arrayUnion({
@@ -93,13 +94,13 @@ export default function Comments({ movie1 }) {
           }),
         });
       } else {
-        alert("Please enter the text");
+        alert('Please enter the text');
       }
     } else {
       history.push(SIGNIN_ROUTE);
     }
 
-    setCommentValue("");
+    setCommentValue('');
   }
 
   useEffect(() => {
@@ -115,16 +116,15 @@ export default function Comments({ movie1 }) {
   return (
     <Box sx={{ flexGrow: 1 }}>
       <Grid container spacing={2}>
-        <Grid item xs={0} md={1}></Grid>
-        <Grid item xs={12} md={10}>
-          <Item sx={{ backgroundColor: "#1F1F1F" }}>
+      <Grid item md={6} xs={12}>
+          <Item sx={{ backgroundColor: '#1F1F1F' }}>
             {!isEmpty && (
               <div className={classes.divComentUsers}>
                 {loadingComentPage.map((comment) => (
                   <div>
                     <div className={classes.comentValueUser}>
-                      <p style={{ color: "black" }}>{comment.displayName}</p>
-                      <p style={{ lineHeight: "20px" }}> {comment.text} </p>
+                      <p style={{ color: 'black' }}>{comment.displayName}</p>
+                      <p style={{ lineHeight: '20px' }}> {comment.text} </p>
                       <span className={classes.spanTime}>
                         {comment.timeCreatedAt}
                       </span>
@@ -135,10 +135,9 @@ export default function Comments({ movie1 }) {
             )}
           </Item>
         </Grid>
-        <Grid item xs={0} md={1}></Grid>
-        <Grid item xs={0} md={2}></Grid>
-        <Grid item xs={9} md={7}>
-          <Item sx={{ backgroundColor: "#1F1F1F" }}>
+
+        <Grid item md={6} xs={12}>
+          <Item sx={{ backgroundColor: '#1F1F1F' }}>
             <TextareaAutosize
               value={commentValue}
               onChange={onHandleChange}
@@ -149,10 +148,8 @@ export default function Comments({ movie1 }) {
               style={{ width: 300 }}
             />
           </Item>
-        </Grid>
-        <Grid item xs={1} md={1}>
-          <Item sx={{ backgroundColor: "#1F1F1F" }}>
-            {comentUserName !== "" && (
+          <Item sx={{ backgroundColor: '#1F1F1F' }}>
+            {comentUserName !== '' && (
               <Button
                 onClick={onAddItem}
                 className={classes.rootBtn}
@@ -161,7 +158,7 @@ export default function Comments({ movie1 }) {
                 Send
               </Button>
             )}
-            {comentUserName == "" && (
+            {comentUserName === '' && (
               <div>
                 <Button
                   onClick={handleClickOpen}
@@ -187,6 +184,44 @@ export default function Comments({ movie1 }) {
               </div>
             )}
           </Item>
+        </Grid>
+        <Grid item xs={1} md={1}>
+          {/* <Item sx={{ backgroundColor: '#1F1F1F' }}>
+            {comentUserName !== '' && (
+              <Button
+                onClick={onAddItem}
+                className={classes.rootBtn}
+                variant="text"
+              >
+                Send
+              </Button>
+            )}
+            {comentUserName === '' && (
+              <div>
+                <Button
+                  onClick={handleClickOpen}
+                  className={classes.rootBtn}
+                  variant="text"
+                >
+                  Send
+                </Button>
+                <Dialog
+                  open={open}
+                  onClose={handleClose}
+                  aria-labelledby="alert-dialog-title"
+                  aria-describedby="alert-dialog-description"
+                >
+                  <DialogContent>
+                    <DialogContentText id="alert-dialog-description">
+                      Please Login or Register to Add Movies/TV Shows to Your
+                      Favorites List.
+                    </DialogContentText>
+                    <Login />
+                  </DialogContent>
+                </Dialog>
+              </div>
+            )}
+          </Item> */}
         </Grid>
       </Grid>
     </Box>
